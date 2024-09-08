2c65709af39419c851c6d1b1bd394c1915add6bd
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-09-08 15:13:42 -0300

    Update marker icon

--- public/js/map.js
+++ public/js/map.js
@@ -7,7 +7,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+const customIcon = L.icon({
+    iconUrl: '../images/icon-location.svg',
+    iconSize: [46, 56],
+    iconAnchor: [23, 56],
+    popupAnchor: [0, -56]
+})
+
 let marker;
+
 
 // Função para atualizar o mapa com a nova localização
 function updateMap(lat, lng) {
@@ -18,6 +26,6 @@ function updateMap(lat, lng) {
     if(marker){
         map.removeLayer(marker)
     }
-    marker = L.marker([lat,lng]).addTo(map)
+    marker = L.marker([lat,lng],{icon: customIcon}).addTo(map)
     map.setView([lat, lng], 13);
 }
