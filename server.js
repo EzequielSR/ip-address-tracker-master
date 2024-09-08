73eaa543f5a390e7044baebf3044bda2e69efcd5
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-09-08 15:58:48 -0300

    update local port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT ||3000;
 
 app.use(express.static('public')); // Serve static files
 
