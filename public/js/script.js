bb497384104664e4c96760b14a3c5c80ce4743d6
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-09-09 16:18:52 -0300

    .

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,21 @@
-// script.js
 document.getElementById('ipForm').addEventListener('submit', async (event) => {
-  event.preventDefault();
+  event.preventDefault(); // Prevent form from submitting the default way
 
-  const ip = document.getElementById('ipInput').value;
+  const ipInput = document.getElementById('ipInput').value;
+  const response = await fetch(`/api/ipinfo?ip=${ipInput}`);
+  const data = await response.json();
 
-  try {
-    const response = await fetch(`/api/ipinfo?ip=${ip}`, {
-      method: 'GET'
-    });
+  if (response.ok) {
+      // Update HTML with IP information
+      document.getElementById('ipAddress').textContent = data.ip;
+      document.getElementById('location').textContent = data.location;
+      document.getElementById('timezone').textContent = data.timezone;
+      document.getElementById('isp').textContent = data.isp;
 
-    const data = await response.json();
-
-    if (response.ok) {
-      document.getElementById('ipAddress').textContent = `${data.ip}`;
-      document.getElementById('location').textContent = `${data.location}`;
-      document.getElementById('timezone').textContent = `${data.timezone}`;
-      document.getElementById('isp').textContent = ` ${data.isp}`;
-
-      // Update Map
-      if (data.lat && data.lng) {
-        updateMap(data.lat, data.lng);
-      } else {
-        console.error('Error: Latitude or Longitude not found');
-        alert('Error: Latitude or Longitude not found');
-      }
-    } else {
-      alert(data.error);
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    alert('An error occurred while fetching the IP information.');
+      // Update the map
+      updateMap(data.lat, data.lng);
+  } else {
+      console.error(data.error);
   }
 });
+
