2aef4b66956b6cead8a872ff688d276701ed2d3c
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-09-10 13:50:25 -0300

    Update test code

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 dotenv.config(); // Load environment variables
 
 const app = express();
 const port = process.env.PORT ||3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('public')); // Serve static files
 
 // Middleware to parse request bodies
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {data: {
+        ip: 'Waiting for IP',
+        location: 'Waiting for location',
+        timezone: 'Waiting for timezone',
+        isp: 'Waiting for ISP'
+    }}); 
+})
 
 // Handle IP info requests
-app.get('/api/ipinfo', async (req, res) => {
-    const ip = req.query.ip;
+app.post('/api/ipinfo', async (req, res) => {
+    const ip = req.body.ip;
 
     if (!ip) {
-        return res.status(400).json({ error: 'IP address is required' });
+        // return res.status(400).json({ error: 'IP address is required' });
+        return res.render('index',{data: null, error: 'IP address is required'})
     }
 
     try {
@@ -25,21 +37,26 @@ app.get('/api/ipinfo', async (req, res) => {
         const data = await response.json();
 
         if (data.error) {
-            return res.status(400).json({ error: data.error.message });
+            // return res.status(400).json({ error: data.error.message });
+            return res.render('index',{data: null, error: data.error.message})
         }
 
         // Respond with IP information
-        res.json({
+        res.render('index',{
+            data:{
             ip: data.ip || 'Not available',
             location: data.location ? `${data.location.city || 'City not available'}, ${data.location.country || 'Country not available'}` : 'Location not available',
             timezone: data.location ? `UTC ${data.location.timezone || 'Timezone not available'}` : 'Timezone not available',
             isp: data.isp || 'ISP not available',
             lat: data.location ? data.location.lat : 0,
             lng: data.location ? data.location.lng : 0
+            },
+            error:null
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while fetching IP information' });
+        // res.status(500).json({ error: 'An error occurred while fetching IP information' });
+        res.render('index',{data: null, error:'An error occurred while fetching IP information'})
     }
 });
 
